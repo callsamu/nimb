77f32e07ca96721ead71265780f3dafa21079b94
callsamu <samuelbree@gmail.com>
2021-12-17 21:26:00 +0000

    Implement minimal Pixie image rendering

--- src/main.nim
+++ src/main.nim
@@ -1,15 +1,49 @@
 import sdl2
+import pixie as pix
 from network import request
-from pixie as pix import nil
+
+const
+  rmask = uint32 0x000000ff
+  gmask = uint32 0x0000ff00
+  bmask = uint32 0x00ff0000
+  amask = uint32 0xff000000
+
+proc makeTexture(renderer: RendererPtr): TexturePtr =
+  var 
+    image = pix.newImage(800, 600)
+    ctx = newContext(image)
+
+  ctx.fillStyle = pix.rgba(0, 0, 0, 255)
+  ctx.fillRect(0, 0, 800, 600)
+  ctx.fillStyle = pix.rgba(0, 0, 255, 255)
+  ctx.fillRect(0, 0, 400, 300)
+
+  var 
+    data = ctx.image.data[0].addr
+    surface: SurfacePtr
+
+  surface = sdl2.createRGBSurfaceFrom(
+    data, cint 800, cint 600, 
+    cint 32, cint 4*800, rmask, 
+    gmask, bmask, amask
+  )
+  
+  result = renderer.createTextureFromSurface(surface)
+    
+
+
 
 proc main(arg: string) =
-  init(INIT_EVERYTHING)
+  sdl2.init(INIT_EVERYTHING)
 
   var
     window = createWindow("test", 0, 0, 800, 600, 0)
     render = window.createRenderer(-1, Renderer_Accelerated)
+    texture = render.makeTexture()
 
   render.setDrawColor(0, 0, 0, 0)
+  render.clear()
+  render.copy(texture, nil, nil)
   render.present()
   delay(4000)
 
